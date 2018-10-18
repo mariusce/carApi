2388df5323196f4ce4b040fca6ce71960fd0b618
Marius Suta <marius@c-scale.ro>
2018-10-18 15:04:47 +0300

    added contact list per user

--- src/models/user.js
+++ src/models/user.js
@@ -4,8 +4,10 @@ const mongoose      = require('mongoose');
 const userCommon    = require('./userCommon');
 const Schema        = mongoose.Schema;
 
+
 let UserSchema = new Schema(_.extend({
-  carNumber: {type: String, unique: true, required: true, index: true}
+  carNumber: {type: String, unique: true, required: true, index: true},
+  contacts: [Schema.ObjectId]
 }, userCommon));
 
 let UserModel = mongoose.model('User', UserSchema);
