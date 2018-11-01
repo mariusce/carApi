d8ee98313c8620e2a3a3e1ca1a35a6ddc2523e87
Marius Suta <marius@c-scale.ro>
2018-11-01 19:23:17 +0200

    Added messages per user contacts

--- src/models/user.js
+++ src/models/user.js
@@ -7,7 +7,20 @@ const Schema        = mongoose.Schema;
 
 let UserSchema = new Schema(_.extend({
   carNumber: {type: String, unique: true, required: true, index: true},
-  contacts: [String]
+  contacts: [{
+    carNumber: {type: String},
+    messages: [{
+      _id: {type: String},
+      text: {type: String},
+      createdAt: {type: Date},
+      user: {
+        _id: {type: String},
+        name: {type: String}
+      },
+      sent: {type: Boolean, default: false},
+      received: {type: Boolean, default: false},
+    }]
+  }]
 }, userCommon));
 
 let UserModel = mongoose.model('User', UserSchema);
